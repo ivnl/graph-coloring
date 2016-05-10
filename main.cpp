34cb74217e09def83515edae22f334c7a247b68c
Ivan <ivansaur@gmail.com>
2016-05-10 13:03:25 -0400

    initial

---
+++ main.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+ifstream in_data1;
+ofstream out_data1;
+using namespace std;
+
+class graphColoring {
+
+public:
+
+    int **adjacencyMatrix;
+    int uNode = 0;
+    int newColor = 0;
+    int numNodes = 0;
+
+    ~graphColoring() {
+        for (int k = 0; k < numNodes; k++) {
+            delete[] adjacencyMatrix[k];
+        }
+        delete[] adjacencyMatrix;
+    }
+
+    graphColoring(ifstream &data) {
+        data >> numNodes; //store first number to numNodes
+        cout << "Number of Nodes: " << numNodes;
+
+        loadMatrix(data);
+        printMatrix();
+        colorGraph();
+    }
+
+    void colorGraph() {
+
+        //STEP 5: repeat steps 1-3 until all nodes are colored
+        while (!checkAllColored()) {
+
+            //STEP 1:
+            newColor++;
+
+            //STEP 2 & 3:
+            for (int j = 0; j < numNodes; j++) {
+                //find uncolored node
+                if (adjacencyMatrix[j][j] == 0) {
+                    if (checkAdjacencyValid(j, newColor)) {
+                        adjacencyMatrix[j][j] = newColor;
+                    }
+                }
+
+            }
+            //STEP 4:
+            printMatrix();
+        }
+
+    }
+
+    bool checkAdjacencyValid(int currentNode, int currentColor) {
+        for (int i = 0; i < numNodes; i++) {
+            if (adjacencyMatrix[currentNode][i] == -1 || adjacencyMatrix[i][currentNode] == -1) {
+                if (adjacencyMatrix[i][i] == currentColor) return false;
+            }
+        }
+        return true;
+    }
+
+    bool checkAllColored() {
+        for (int i = 0; i < numNodes; i++) {
+            if (adjacencyMatrix[i][i] == 0) return false;
+        }
+        return true;
+    }
+
+    void printMatrix() {
+        for (int i = 0; i < numNodes; i++) {
+            out_data1 << endl;
+            for (int k = 0; k < numNodes; k++) {
+                if (adjacencyMatrix[i][k] == -1) {
+                    out_data1 << setw(4) << "X" << " ";
+                } else
+                    out_data1 << setw(4) << adjacencyMatrix[i][k] << " ";
+            }
+        }
+        out_data1 << endl << endl << endl;
+
+
+    }
+
+    void loadMatrix(ifstream &data) {
+
+        int row = 0, col = 0;
+
+        adjacencyMatrix = new int *[numNodes]();
+
+        for (int i = 0; i < numNodes; ++i) {
+            adjacencyMatrix[i] = new int[numNodes]();
+        }
+
+        while (!data.eof()) {
+            data >> row;
+            data >> col;
+            adjacencyMatrix[row - 1][col - 1] = -1;
+            adjacencyMatrix[col - 1][row - 1] = -1;
+        }
+    }
+};
+
+int main(int argc, char *argv[]) {
+
+    in_data1.open(argv[1]);
+    out_data1.open(argv[2]);
+
+    graphColoring graph(in_data1);
+
+    in_data1.close();
+    out_data1.close();
+
+    return 0;
+}
